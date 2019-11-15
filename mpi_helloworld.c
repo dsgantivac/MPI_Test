1578ee3b16ffa17e1ddf930e6a6fff1d3414e32b
Ubuntu <gantiva@node-master.tqfjygx2r0vujlld0gxzbouhcc.bx.internal.cloudapp.net>
2019-11-15 00:03:18 +0000

    azure change

--- mpi_helloworld.c
+++ mpi_helloworld.c
@@ -6,7 +6,7 @@ int main(int argc, char **argv ) {
 	MPI_Init( &argc, &argv );
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	printf("Hello world from process %d of %d\n", rank, size );
+	printf("kio padre world from process %d of %d\n", rank, size );
 	MPI_Finalize( );
 return 0;
 }
